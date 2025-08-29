5b3432b64ab2cd44ee1d2520f81238a6ac72f550
villno55 <adsotrabajos24@gmail.com>
2025-08-29 11:55:30 -0500

    unido

--- unidad3Ejer1.js
+++ unidad3Ejer1.js
@@ -1,20 +1,42 @@
 class trabajador {
-  #suel; 
+  #suel;  
+  #correo; 
 
-  constructor(nombre, sueldo) {
-    this.nom = nombre;
-    this.#suel = sueldo;
-  }
+  constructor(nombre, sueldo, correo) {
+    this.nom = nombre;
+    this.#suel = sueldo;
+    this.correo = correo; 
+  }
 
-  aplicoAumento(porcentaje) {
-    this.#suel += this.#suel * porcentaje / 100;
-  }
+  aplicoAumento(porcentaje) {
+    this.#suel += this.#suel * porcentaje / 100;
+  }
 
-  verInfo() {
-    console.log("el empleado:", this.nom, "tiene este sueldo:", this.#suel);
-  }
+  verInfo() {
+    console.log(`Empleado: ${this.nom}`);
+    console.log(`Sueldo: ${this.#suel.toFixed(2)}`);
+    console.log(`Correo: ${this.#correo}`);
+  }
+
+  get correo() {
+    return this.#correo;
+  }
+
+  
+  set correo(valor) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (regex.test(valor)) {
+      this.#correo = valor;
+    } else {
+      console.error(`Correo inválido: ${valor}`);
+    }
+  }
 }
 
-const emp1 = new trabajador("Daniel Mbappe", 2000);
+
+const emp1 = new trabajador("Daniel Mbappé", 2000, "daniel.mbappe@example.com");
 emp1.aplicoAumento(10);
+emp1.verInfo();
+
+emp1.correo = "correo-invalido";  
 emp1.verInfo();
