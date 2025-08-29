963302eaebd8643a264b31f589c423afa06d1406
villno55 <adsotrabajos24@gmail.com>
2025-08-29 10:14:16 -0500

    Update unidad1Ejer2.js

--- unidad1Ejer2.js
+++ unidad1Ejer2.js
@@ -1,1 +1,14 @@
-console.log("Unidad 1 - Ejercicio 2");
+class student {
+  constructor(nombre, materias) {
+    this.nombre = nombre;
+    this.materias = materias;
+  }
+
+  listarMaterias() {
+    console.log("2) Materias de " + this.nombre + ": " + this.materias.join(", "));
+  }
+}
+
+const e1 = new student("pacho", ["arq.software", "js", "fundamentos"]);
+e1.listarMaterias();
+
