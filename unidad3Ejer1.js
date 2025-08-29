1cc64d7110d99159ea6ac1e3a3da06360fea2dc4
villno55 <adsotrabajos24@gmail.com>
2025-08-29 10:33:30 -0500

    changedvar

--- unidad3Ejer1.js
+++ unidad3Ejer1.js
@@ -1,1 +1,20 @@
-console.log("Unidad 3 - Ejercicio 1");
+class trabajador {
+  #suel; 
+
+  constructor(nombre, sueldo) {
+    this.nom = nombre;
+    this.#suel = sueldo;
+  }
+
+  aplicoAumento(porcentaje) {
+    this.#suel += this.#suel * porcentaje / 100;
+  }
+
+  verInfo() {
+    console.log("el empleado:", this.nom, "tiene este sueldo:", this.#suel);
+  }
+}
+
+const emp1 = new trabajador("Daniel Mbappe", 2000);
+emp1.aplicoAumento(10);
+emp1.verInfo();
