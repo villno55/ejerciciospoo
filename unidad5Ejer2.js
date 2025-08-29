4f2a1768d86e64c340849c6e2434874e146c5723
villno55 <adsotrabajos24@gmail.com>
2025-08-29 11:53:59 -0500

    add file

--- unidad5Ejer2.js
+++ unidad5Ejer2.js
@@ -1,1 +1,45 @@
-console.log("Unidad 5 - Ejercicio 2");
+class Autor {
+  constructor(nom, nacimi) {
+    this.nom = nom;
+    this.nacimi = nacimi;
+  }
+
+  verInfo() {
+    console.log(`Autor: ${this.nom} (${this.nacimi})`);
+  }
+}
+
+class Editorial {
+  constructor(nom, city) {
+    this.nom = nom;
+    this.city = city;
+  }
+
+  verInfo() {
+    console.log(`Editorial: ${this.nom} - Ciudad: ${this.city}`);
+  }
+}
+
+class Libro {
+  constructor(titulo, autor, edito) {
+    this.titulo = titulo;
+    this.autor = autor;
+    this.edito = edito;
+  }
+
+  verInfo() {
+    this.autor.verInfo();
+    console.log(`Libro: ${this.titulo}`);
+    this.edito.verInfo();
+  }
+}
+
+
+const aut1 = new Autor("Sthepen King", "EE.UU");
+const edito1 = new Editorial("PREHISTON", "MANHATTAN");
+
+const lib1 = new Libro("EL RESPLANDOR", aut1, edito1);
+
+
+lib1.verInfo();
+
