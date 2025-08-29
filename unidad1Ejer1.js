4ee13cafeb9acb763af6a388cac73b6765e05b9c
villno55 <adsotrabajos24@gmail.com>
2025-08-29 09:38:24 -0500

    Update

--- unidad1Ejer1.js
+++ unidad1Ejer1.js
@@ -1,1 +1,13 @@
-console.log("Unidad 1 - Ejercicio 1");
+class Circulo {
+  constructor(radio) {
+    this.radio = radio;
+  }
+
+  calcularArea() {
+    return 3.14 * this.radio * this.radio;
+  }
+}
+
+const c1 = new Circulo(5);
+console.log("1) Área del círculo:", c1.calcularArea());
+
