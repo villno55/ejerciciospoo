2ae27bc9917dd5bb6d5c853dd20845cf9a746c10
villno55 <adsotrabajos24@gmail.com>
2025-08-30 11:57:25 -0500

    Update unidad3Ejer2.js

--- unidad3Ejer2.js
+++ unidad3Ejer2.js
@@ -1,1 +1,50 @@
-console.log("Unidad 3 - Ejercicio 2");
+/*ejer4.1bien hecho
+class Persona {
+  constructor(id, nombre, correo) {
+    this.id = id;
+    this.nombre = nombre;
+    this.correo = correo;
+  }
+
+  mostrarInfo() {
+    console.log("ID:", this.id, "Nombre:", this.nombre, "Correo:", this.correo);
+  }
+}
+
+class Instructor extends Persona {
+  constructor(id, nombre, correo, especialidad) {
+    super(id, nombre, correo);
+    this.especialidad = especialidad;
+  }
+
+  mostrarInfo() {
+    console.log("ID:", this.id, "Nombre:", this.nombre, "Correo:", this.correo, "Especialidad:", this.especialidad);
+  }
+}
+
+class Aprendiz extends Persona {
+  constructor(id, nombre, correo, puntaje) {
+    super(id, nombre, correo);
+    this.puntaje = puntaje;
+  }
+
+  mostrarInfo() {
+    console.log("ID:", this.id, "Nombre:", this.nombre, "Correo:", this.correo, "Puntaje ICFES:", this.puntaje);
+  }
+}
+
+// Pruebas
+const profe = new Instructor(1, "pacho", "pacho10@gmail.com", "software");
+const estudiante = new Aprendiz(2, "Daniel Mbappe", "MBAPEREZ@GMAIL.com", 298);
+
+profe.mostrarInfo();
+estudiante.mostrarInfo();
+
+
+*/
+
+
+/*ejer4.1repetido
+
+
+*/
